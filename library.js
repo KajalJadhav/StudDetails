68d56feb3dfb52a57680dffa50947f642b8cc2ec
unknown <kajaljad@INKAJALJAD.corporate.thoughtworks.com>
2013-11-12 17:19:22 +0530

    Library modified

--- library.js
+++ library.js
@@ -1,19 +1,27 @@
 var http = require('http');
 var fs = require("fs");
-var data,id,sum = 0;
-var list = function(fileName){
-	var data = readFileSync(fileName);
-	return data;
-}
+var sd = {};
+sd.list = function(filename){
+	var records = fs.readFileSync(filename);
+	var field = ["RollNo  Name  Percentage\r\n"];
+	if(records == '{}') return 'No records available';
+	keysInRecords = Object.keys(JSON.parse(records));
+	var records = JSON.parse(records);
+	var result = [],index = 0;
+	keysInRecords.forEach(function(fields){
+		result[index] = records[fields].rn+'   '+records[fields].nm+'   '+records[fields].pr;
+		index++;
+	});
+	return field+result.join('\r\n');
+};
 var display = function(request, response) {
   response.writeHead(200, {"Content-Type": "text/plain"});
   data = request.url;
   id = require('url').parse(data,true);
   var path = id.pathname.substr(1);
   if(path == "list"){
-  	response.write(list("record.txt"));
+  	response.write(sd.list("record.txt"));
   }
-  // response.write(data+"====="+JSON.stringify(id));
   response.end();
 }
-http.createServer(display).listen(8282);
+http.createServer(display).listen(8082);
