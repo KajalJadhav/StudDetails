4be67027426e2e3e787a2e9dff941d7bccbacc77
Kajal <kajal@inankhome.com>
2013-11-12 18:35:54 +0530

    Search is added to Library

--- library.js
+++ library.js
@@ -32,16 +32,44 @@ sd.list = function(text){
   return field+result.join('\r\n');
 };
 
+sd.searchRecord = function(record,search,field){
+  if(field == 'RollNo') field = 'RollNo'
+  var data = JSON.parse(record);
+  if(!search) return "Please specify record to be searched";
+  var keysInRecord = Object.keys(data);
+  var recordFields = ["RollNo", "Name", "Percentage"];
+  if(field == null){
+    var filteredKeys = keysInRecord.filter(function (key){
+      return recordFields.some(function (field){
+        var detail = !data[key][field] ? "" : data[key][field];
+        return detail.toLowerCase().toString().indexOf(search.toLowerCase()) > -1
+      });
+    });
+  }
+  else
+  {
+    var filteredKeys = keysInRecord.filter(function(key){
+      var detail = !data[key][field] ? "" : data[key][field];
+      return detail.toLowerCase().toString().indexOf(search.toLowerCase()) > -1;
+    });
+  }
+  var result = {}; 
+  for (var counter = 0; counter < filteredKeys.length; counter++)
+    result[filteredKeys[counter]] = data[filteredKeys[counter]];
+  var result = sd.list(JSON.stringify(result));
+  return result;    
+};
+
 sd.perform =  function(data,methodName){
   console.log(methodName);
   var text = sd.readData("record.txt");
   var operations = {
     add : function(){var res = {data : sd.addDetail(data.rn,data.name,data.percentage)};
                     return sd.list(JSON.stringify(res));},
-    list : function(){return sd.list(text);}
+    list : function(){return sd.list(text);},
+    search : function(){return sd.searchRecord(text,data.rn,'RollNo');}
   };
   
   return operations[methodName]();
 };
-
 exports.sd = sd;
