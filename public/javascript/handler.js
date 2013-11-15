8e0016ed2b5d999b234bf54af9da38db0863511c
Manali <manu@RLB.com>
2013-11-15 17:00:09 +0530

    with Asynchronus and Client side validations

--- public/javascript/handler.js
+++ public/javascript/handler.js
@@ -4,23 +4,50 @@ var home_page = fs.readFileSync('./public/html/index.html');
 var add = fs.readFileSync('./public/html/add.html');
 var search = fs.readFileSync('./public/html/search.html');
 var sd = require('./library.js').sd;
+var recordFileName = './public/text/record.txt';
 
 var data = {rn:''};
+
 routes['/'] = function(req,res){
-	return home_page.toString();
+	res.writeHead(200, {"Content-Type": "text/html"});
+	res.write(""+home_page);
+	res.end();
 }
+
 routes['/list'] = function(req,res){
-	return sd.perform('','list');
-}
+	res.writeHead(200, {"Content-Type": "text/html"});
+	var onReadData = function(err,text){
+		if (err) throw err;
+		res.write(sd.list(text));
+		res.end();
+	};
+	fs.readFile(recordFileName,'utf-8',onReadData);
+};
+
 routes['/add'] = function(req,res){
-	if(!data.rn)
-		return add.toString();
-	return sd.perform(data,'add');
+	res.writeHead(200, {"Content-Type": "text/html"});
+	if(!data.rn){
+		res.write(add);
+		res.end()
+		return;
+	}
+	res.write(sd.addDetail(data.rn,data.name,data.percentage));
+	res.end();
 }
 routes['/search'] = function(req,res){
-	if(!data.rn)
-		return search.toString();
-	return sd.perform(data,'search');
+	res.writeHead(200, {"Content-Type": "text/html"});
+	if(!data.rn){
+		res.write(search);
+		res.end()
+		return;
+	}
+	var onReadData = function(err,text){
+		if(err) throw err;
+		var result = sd.searchRecord(text,data.rn);
+		res.write(result);
+		res.end();
+	};
+	fs.readFile(recordFileName,'utf-8',onReadData);
 }
 exports.data = data;
 exports.routes = routes;
--- server.js
+++ server.js
@@ -1,23 +1,26 @@
 var http = require('http');
 var url = require('url');
 var sd = require('./public/javascript/library.js').sd;
-var routes = require('./public/javascript/handler').routes;
-var data = require('./public/javascript/handler').data;
+var handler = require('./public/javascript/handler'); 
+var routes = handler.routes;
+var data = handler.data;
 
 var no_method = function(req,res){
-	return sd.perform();
+	res.writeHead(404, {"Content-Type": "text/html"});
+	var msg = "The available functionalities are<br/> 1. Add <br/> 2. List <br/> 3. Search";
+	res.write(msg);
+	res.end();
 };
 var ConnectionListener = function (req,res){
 	var requrl = url.parse(req.url,true);
-	res.writeHead(200, {"Content-Type": "text/html"});
 	data.rn = requrl.query.roll;
 	data.name = requrl.query.name;
 	data.percentage = requrl.query.per;
 	var method = requrl.pathname;
 	var main_route = routes[method] || no_method;
-	res.write("<h3>" + main_route(req,res)+ "</h3>");
-	res.end();
+	main_route(req,res);
 };
 var server = http.createServer(ConnectionListener);
 server.listen(8088);
 console.log('server is listening at port 8088');
+
