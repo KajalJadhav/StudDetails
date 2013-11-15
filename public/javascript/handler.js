1ae66a014de007dd9c1272e2840ee2be33b3b500
unknown <shitalma@Inshitalma.corporate.thoughtworks.com>
2013-11-15 21:01:14 +0530

    css file added 15-11-13

--- public/javascript/handler.js
+++ public/javascript/handler.js
@@ -3,24 +3,53 @@ var fs = require('fs');
 var home_page = fs.readFileSync('./public/html/index.html');
 var add = fs.readFileSync('./public/html/add.html');
 var search = fs.readFileSync('./public/html/search.html');
+var backgroundImage = fs.readFileSync('./public/images/students.jpg');
+var bg = fs.readFileSync('./public/images/image.jpg');
+var studentCSS = fs.readFileSync('./public/css/studentDetails.css');
 var sd = require('./library.js').sd;
 
 var data = {rn:''};
+var res = {content:'',format:'text/html'};
 routes['/'] = function(req,res){
-	return home_page.toString();
+	res.content = home_page.toString();
+	res.format = 'text/html';
+	return res;
 }
 routes['/list'] = function(req,res){
-	return sd.perform('','list');
+	res.content = sd.perform('','list');
+	res.format = 'text/html';
+	return res;
 }
 routes['/add'] = function(req,res){
 	if(!data.rn)
-		return add.toString();
-	return sd.perform(data,'add');
+		res.content = add.toString();
+	res.content = sd.perform(data,'add');
+	res.format = 'text/html';
+	return res;
 }
 routes['/search'] = function(req,res){
 	if(!data.rn)
-		return search.toString();
-	return sd.perform(data,'search');
+		res.content = search.toString();
+	res.content = sd.perform(data,'search');
+	res.format = 'text/html';
+	return res;
+}
+routes['/students.jpg'] = function(req,res){
+	res.content = backgroundImage;
+	res.format = 'image/jpeg';
+	// console.log(backgroundImage);
+	return res;
+}
+routes['/image.jpg'] = function(req,res){
+	res.content = bg;
+	res.format = 'image/jpeg';
+	// console.log(backgroundImage);
+	return res;
+}
+routes['/studentDetails.css'] = function(req,res){
+	res.content = studentCSS;
+	res.format = 'text/css';
+	return res;
 }
 exports.data = data;
 exports.routes = routes;
--- server.js
+++ server.js
@@ -5,17 +5,17 @@ var routes = require('./public/javascript/handler').routes;
 var data = require('./public/javascript/handler').data;
 
 var no_method = function(req,res){
-	return sd.perform();
+	return {content:sd.perform(),format:'text/html'};
 };
 var ConnectionListener = function (req,res){
 	var requrl = url.parse(req.url,true);
-	res.writeHead(200, {"Content-Type": "text/html"});
 	data.rn = requrl.query.roll;
 	data.name = requrl.query.name;
 	data.percentage = requrl.query.per;
 	var method = requrl.pathname;
-	var main_route = routes[method] || no_method;
-	res.write("<h3>" + main_route(req,res)+ "</h3>");
+	var main_route_res = routes[method] || no_method;
+	res.writeHead(200, {"Content-Type": main_route_res(req,res).format});
+	res.write(main_route_res(req,res).content);
 	res.end();
 };
 var server = http.createServer(ConnectionListener);
